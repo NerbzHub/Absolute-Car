bad3c639e9537f662426256ee37f93b958426d3b
NerbzHub <nathan.nette@gmail.com>
2017-09-06 15:25:16 +1000

    Initial Commit
    Assets and Project Settings

---
+++ Assets/Scripts/scrpCamera1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrpCamera1 : MonoBehaviour {
+
+    //var camTarget
+    //Vector3 camTarget;
+    // Use this for initialization
+    public int DistanceAwayX = 0;
+    public int DistanceAwayY = 0;
+    public int DistanceAwayZ = 0;
+    
+    void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        
+        Vector3 PlayerPOS = GameObject.Find("CarModel").transform.transform.position;
+        GameObject.Find("Main Camera").transform.position = new Vector3(PlayerPOS.x + DistanceAwayX, PlayerPOS.y + DistanceAwayY, PlayerPOS.z - DistanceAwayZ);
+    }
+}
---
+++ Assets/Scripts/scrpPlayer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrpPlayer : MonoBehaviour {
+
+	//A public float for speed.
+	public float speedP1 = 1.0f;
+	public GameObject m_BulletPrefab = null;
+	public int bulletCountP1 = 0;
+
+    ////Camera
+    //public float cameraDistOffset = 10;
+    //private Camera player1Cam;
+    //private GameObject player1;
+
+	// Use this for initialization
+	void Awake ()
+	{
+        //player1Cam = GetComponent<Camera>();
+        //player1 = GameObject.Find("Player1")
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log(bulletCountP1);
+
+		Vector3 pos;
+		pos.x = transform.position.x;
+
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(Vector3.forward * speedP1 * Time.deltaTime);
+		}
+			
+		if(Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(Vector3.back * speedP1 * Time.deltaTime);
+		}
+
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(0, Time.deltaTime * 200.0f, 0);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(0, Time.deltaTime * -200.0f, 0);
+		}
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			GameObject copy = Instantiate(m_BulletPrefab);
+			copy.transform.position = transform.position + transform.forward;
+
+			Rigidbody rb = copy.GetComponent<Rigidbody>();
+			rb.AddForce(transform.forward * 1000, ForceMode.Acceleration);
+			++bulletCountP1;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+}
