87907368d29aa14c58088886163f914d134ca394
NerbzHub <nathan.nette@gmail.com>
2017-09-11 22:07:43 +1000

    Splashscreen
    I set up a splashscreen to run for 3 seconds in a different scene when the game is loaded.

---
+++ Assets/Scripts/scrp_SplashScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scrp_SplashScreen : MonoBehaviour {
+
+    public float timer = 3f;
+    public string levelToLoad = "Level01";
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine("DisplayScene");
+	}
+	
+    IEnumerator DisplayScene()
+    {
+        yield return new WaitForSeconds(timer);
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
